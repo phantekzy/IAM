a62f33f3f7235e1279f2a04cf9278d8d4cd97f1e
phantekzy <mainilotfi@gmail.com>
2026-05-27 16:04:25 +0100

    theme section is done

--- src/theme.rs
+++ src/theme.rs
@@ -51,3 +51,21 @@ pub fn tinted_card(tint: Color32, alpha: u8) -> Color32 {
     let blend = |base: u8, t: u8| -> u8 { ((base as u16 * (255 - a) + t as u16 * a) / 255) as u8 };
     Color32::from_rgb(blend(br, tr), blend(bg, tg), blend(bb, tb))
 }
+
+pub fn set_theme(dark: bool) {
+    if dark {
+        T_BG.with(|c| c.set([12, 12, 15]));
+        T_CARD.with(|c| c.set([25, 25, 32]));
+        T_CARD2.with(|c| c.set([35, 35, 45]));
+        T_BORDER.with(|c| c.set([50, 50, 65]));
+        T_TEXT.with(|c| c.set([240, 240, 245]));
+        T_MUTED.with(|c| c.set([130, 130, 145]));
+    } else {
+        T_BG.with(|c| c.set([242, 243, 247]));
+        T_CARD.with(|c| c.set([255, 255, 255]));
+        T_CARD2.with(|c| c.set([235, 236, 242]));
+        T_BORDER.with(|c| c.set([200, 202, 215]));
+        T_TEXT.with(|c| c.set([20, 20, 30]));
+        T_MUTED.with(|c| c.set([90, 90, 110]));
+    }
+}
