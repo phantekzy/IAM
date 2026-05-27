5f2c351e909139fe6be53f0723391b5676fca06f
phantekzy <mainilotfi@gmail.com>
2026-05-27 16:06:47 +0100

    helper of ui section

---
+++ src/ui_helpers.rs
@@ -0,0 +1,3 @@
+use chrono::{Duration, Local, NaiveDate};
+use eframe::egui::{self, Color32, FontId, Margin, RichText, Rounding, Stroke};
+use theme::{border, card, card2, muted, text, RED_IAM, WHITE};
