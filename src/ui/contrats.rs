380e229ca45a562e89baaf0fdda23bf75362f974
phantekzy <mainilotfi@gmail.com>
2026-05-27 17:15:01 +0100

    contracts section

---
+++ src/ui/contrats.rs
@@ -0,0 +1,6 @@
+use app::App;
+use eframe::egui::{self, RichText};
+use models::Onglet;
+use storage::{rentals_file, sauvegarder};
+use theme::{card2, muted, AMBER, GREEN, RED_IAM};
+use ui_helpers::*;
