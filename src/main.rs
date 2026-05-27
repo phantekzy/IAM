4d3754df9924ad27aec861f3f61ccd9443cd0ff5
phantekzy <mainilotfi@gmail.com>
2026-05-27 16:37:05 +0100

    idk what it doesnt count the commits

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,21 @@ mod app;
 mod models;
 mod storage;
 mod theme;
+mod ui;
 mod ui_helpers;
+
+use app::App;
+
+fn main() -> eframe::Result<()> {
+    eframe::run_native(
+        "IAM Business",
+        eframe::NativeOptions {
+            viewport: eframe::egui::ViewportBuilder::default()
+                .with_title("IAM Business - Gestion Location de Voitures by phantekzy")
+                .with_inner_size([1200.0, 860.0])
+                .with_min_inner_size([1100.0, 700.0]),
+            ..Default::default()
+        },
+        Box::new(|_cc| Ok(Box::new(App::new()))),
+    )
+}
