6cf1f5d4d51b3b7b3173d1d56aa70ea09b2f205d
phantekzy <mainilotfi@gmail.com>
2026-06-02 20:49:50 +0100

    i want to improuve the dekstop app with new features

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-mod app;
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod models;
 mod storage;
 mod theme;
-mod ui;
-mod ui_helpers;
+mod utils;
+mod app;
 
 use app::App;
 
@@ -11,8 +12,8 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "IAM Business",
         eframe::NativeOptions {
-            viewport: eframe::egui::ViewportBuilder::default()
-                .with_title("IAM Business - Gestion Location de Voitures by phantekzy")
+            viewport: egui::ViewportBuilder::default()
+                .with_title("IAM Business  Gestion Location de Voitures by phantekzy")
                 .with_inner_size([1200.0, 860.0])
                 .with_min_inner_size([1100.0, 700.0]),
             ..Default::default()
@@ -20,3 +21,4 @@ fn main() -> eframe::Result<()> {
         Box::new(|_cc| Ok(Box::new(App::new()))),
     )
 }
+
